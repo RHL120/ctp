8d534ea21e495aa1ccee9a3c23abafdddefa2c82
rhl120 <rhl120@protonmail.com>
2022-10-02 20:05:23 +0200

    Add some comments

--- part_one/chapter1/q1_to_q3.hs
+++ part_one/chapter1/q1_to_q3.hs
@@ -1,12 +1,15 @@
 module Main where
 
 --I know the book said to not use haskell but I simply can not resist
+--question 1
 myId :: a -> a
 myId x = x
 
+--question 2
 myCompose :: (a -> b) -> (b -> c) -> (a -> c)
 myCompose f g = \x -> g (f x)
 
+--question 3
 addOne :: Int -> Int
 addOne = (1 +)
 
