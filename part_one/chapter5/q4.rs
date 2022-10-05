f718f66c1d716e12a38e4a6f3f98fa3eda429cd8
rhl120 <rhl120@protonmail.com>
2022-10-05 15:26:08 +0200

    Solution to question 5 of chapter 5

--- part_one/chapter5/q4.rs
+++ part_one/chapter5/q4.rs
@@ -4,4 +4,29 @@ enum Either<L, R> {
     Right(R),
 }
 
+//Question 5:
+fn i(n: i32) -> i32 {
+    n
+}
+fn j(b: bool) -> i32 {
+    if b {
+        0
+    } else {
+        1
+    }
+}
+
+fn m(e: Either<i32, bool>) -> i32 {
+    match e {
+        Either::Left(x) => x,
+        Either::Right(x) => {
+            if x {
+                0
+            } else {
+                1
+            }
+        }
+    }
+}
+
 fn main() {}
