4e52099b871e1d4fbf69dab0169bc8d39b7a11fd
rhl120 <rhl120@protonmail.com>
2022-10-05 15:36:24 +0200

    Solution to question 7 of chapter 4

--- part_one/chapter5/q4.rs
+++ part_one/chapter5/q4.rs
@@ -29,4 +29,36 @@ fn m(e: Either<i32, bool>) -> i32 {
     }
 }
 
+//Question 6: I did not understand the point of this question we already proved that
+//it is a bad candidate in question 5
+
+//Question 7:
+fn i2(n: i32) -> i32 {
+    if n < 0 {
+        n
+    } else {
+        n + 2
+    }
+}
+fn j2(b: bool) -> i32 {
+    if b {
+        0
+    } else {
+        1
+    }
+}
+
+fn m2(e: Either<i32, bool>) -> i32 {
+    match e {
+        Either::Left(x) => {
+            if x < 0 {
+                x
+            } else {
+                x - 2
+            }
+        }
+        Either::Right(_) => m2(e),
+    }
+}
+
 fn main() {}
