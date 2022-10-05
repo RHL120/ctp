3dc0258018ffaa55bb476d815b6d3ebec1b9171d
rhl120 <rhl120@protonmail.com>
2022-10-05 18:43:08 +0200

    Solution to questions 3 and 4 of chapter 6

--- part_one/chapter6/q2.rs
+++ part_one/chapter6/q2.rs
@@ -1,12 +1,26 @@
-//Question2: Create a shape
+//Question 2 and 4: Create a shape
 enum Shape {
     Circle(f32),
     Rect(f32, f32),
+    Square(f32),
 }
 
 fn area(s: Shape) -> f32 {
     match s {
-        Shape::Circle(x) => 3.14 * x * *2,
+        Shape::Circle(x) => 3.14 * x.powf(2.0),
         Shape::Rect(x, y) => x * y,
+        Shape::Square(x) => x.powf(2.0),
     }
 }
+
+//Question 3 and 4:
+
+fn circ(s: Shape) -> f32 {
+    match s {
+        Shape::Circle(x) => 2.0 * 3.14 * x,
+        Shape::Rect(x, y) => 2.0 * (x + y),
+        Shape::Square(x) => 4.0 * x,
+    }
+}
+
+fn main() {}
